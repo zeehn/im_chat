4f0f668ee2caac198e2916c40e8706317cd95113
Zakir Hussain <zack.mesacc@gmail.com>
2020-11-16 12:11:43 +0500

    Create authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController 
   def new 
+    @user = User.new
+  end
+  
+  def create 
+    user = User.find_by(username: params[:session][:username])
+    if user and user.authenticate(params[:session][:password])
+      flash[:success] = "Logged in successfully"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Invalid credentials. Try again!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
