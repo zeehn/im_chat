1b2bc9ab8bcef245420ae5aeeb30e2f913832f38
Zakir Hussain <zack.mesacc@gmail.com>
2020-11-16 23:35:31 +0500

    Add realtime chat

--- app/assets/javascripts/channels/chatroom.coffee
+++ app/assets/javascripts/channels/chatroom.coffee
@@ -6,4 +6,4 @@ App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    alert data.message
+    $('#message_container').append(data.message)
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,16 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      ActionCable.server.broadcast "chatroom_channel", message: message.body
+      ActionCable.server.broadcast "chatroom_channel", message: message_render(message)
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
